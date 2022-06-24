dc69b803692cf34c88c03a5404d0885fd10ba8e8
mdsaizraja <mdraja883@gmail.com>
2022-06-24 23:49:41 +0530

    main

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
 
-function App() {
+import "./App.css";
+import { useState } from "react";
+
+export default function App() {
+  const [search, setSearch] = useState(" ");
+  const [api, setApi] = useState({});
+ 
+
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    if (api === "") return;
+    const endpoint = `https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=15&prop=extracts|pageimages&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&origin=*&gsrsearch=${search}`;
+    const response = await fetch(endpoint);
+
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+    const json = await response.json();
+
+    setApi(json.query.pages);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header>
+        <h1>Wiki App</h1>
+        <form className="searchField" onSubmit={handleSearch}>
+          <input placeholder="search" value={search} onChange={(e) => setSearch(e.target.value)} />
+        </form>
+      </header> 
+      <div className="lists">
+        {Object.entries(api).map(([key, item], i) => {
+          const url = `https://en.wikipedia.org/?curid=${item.pageid}`
+          return (
+            <div className="list" key={i}>
+              <h3>{item.title}</h3>
+
+              <p>{item.extract}</p>
+              <a href={url}>Read more</a>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
-
-export default App;
